f1a7a61ca9a7ad9a7b86ecb9586a2851101a5f35
Nicko <nicolasmathieu31@hotmail.com>
2017-01-14 11:13:40 +0100

    Update 2017

--- index-step4.js
+++ index-step4.js
@@ -4,58 +4,40 @@ var vie;
 var nbraleat;
 
 function main(){
-
-	function reset(){
+	function partieInit(){
 		vie = parseInt(prompt("Choisissez le nombre de vie"));
 		$("span").text(vie);
 		var min = parseInt(prompt("Choisissez un nombre aléatoire minimal"));
 		var max = parseInt(prompt("Choisissez un nombre aléatoire maximal"));
-		nbraleat = Math.floor(Math.random() * (max - min + 1)) + min;
-
+		nbraleat = Math.round(Math.random() * (max-min+1))+min;
+		console.log(nbraleat);
 	}
-
 	function partieGagnee(message){
-
 		alert(message);
-		reset();
+		partieInit();
 	}
-
 	function partiePerdue(message){
 
 		alert(message);
 		vie--;
 		$("span").text(vie);
 	}
-
 	function clickValider(){
-
 		var taper = $("input").val();
-
-		if (taper === nbraleat){
+		if (taper == nbraleat){
 			partieGagnee("Gagné");
-			
-		}if(vie === 0){
+		}else if(vie === 0){
 			alert("You loose!")
-			reset();
-
+			partieInit();
 		}else if(taper > nbraleat){
 			partiePerdue("Try again, votre nombre est trop grand");
-			
-
 		}else {
 			partiePerdue("Try again, votre nombre est trop petit");
-			
 		}	
-
 	}
-
 	$("button").click(function(){
-
 		clickValider()
-
 	});
-
-	reset();
-
+	partieInit();
 }
 
